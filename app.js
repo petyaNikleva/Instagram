8566aef9e23f76dc58ca5acd17b08618b8e20e21
P N <petyanikleva@yahoo.com>
2022-05-05 18:49:20 +0300

    implemented class Router

--- app.js
+++ app.js
@@ -4,21 +4,23 @@ import { getAdvertismentView } from './pages/advertisment/advertisment.js';
 import { getFooterView } from './pages/footer/footer.js';
 
 
-import { addNavHtml, addToRoute, hashChangeHandler } from './router/router.js';
+import { Router } from './router/router.js';
 import { newsFeedTemplate } from './pages/newsFeed/newsFeedTemplate.js';
 import { registerTemplate } from './pages/register/registerTemplate.js';
 import { loginTemplate } from './pages/login/loginTemplate.js';
 import { userListTemplate } from './pages/user-list/userListTemplate.js';
 
-addToRoute('/', newsFeedTemplate);
-addToRoute('/#register', registerTemplate);
-addToRoute('/#login', loginTemplate);
-addToRoute('/#user-list', userListTemplate);
+const router = new Router();
+router.addToRoute('/', newsFeedTemplate);
+router.addToRoute('/#register', registerTemplate);
+router.addToRoute('/#login', loginTemplate);
+router.addToRoute('/#user-list', userListTemplate);
 
-addNavHtml(navTemplate); 
+router.addNavHtml(navTemplate);
+
 
 getHeaderView();
 getAdvertismentView();
 getFooterView();
 
-hashChangeHandler(window.location.hash);
+
--- router/router.js
+++ router/router.js
@@ -1,43 +1,79 @@
 import { render } from "../../node_modules/lit-html/lit-html.js";
+import { newsFeedTemplate} from "../pages/newsFeed/newsFeedTemplate.js"
+import { loginTemplate } from "../pages/login/loginTemplate.js";
+import {registerTemplate} from "../pages/register/registerTemplate.js";
 
-let navTemplate = {};
+//let navTemplate = {};
 
-const routes = {
-    // "/": newsFeedTemplate,
-    // "/#register": registerTemplate,
-};
+export class Router {
+    constructor() {
+        this.routes = {};
+        this.navTemplate = {};
+        this.addToRoute('/', newsFeedTemplate);
+        this.addToRoute('/#login', loginTemplate);
+        this.addToRoute('/#register', registerTemplate);
 
-export const addNavHtml = (template) => {
-    navTemplate = template;
+        this.addNavHtml(this.navTemplate);
+
+        this.hashChangeHandler();
+        window.addEventListener('hashchange', this.hashChangeHandler)
+    }
+
+    addToRoute(url, template) {
+        this.routes[url] = template;
+    }
+
+    addNavHtml(template) {
+        this.navTemplate = template;
+    }
+
+    hashChangeHandler() {
+        const locationHash = window.location.hash;
+        //const locationHash = e.path[0].location.hash;
+        const page = `/${locationHash}`;
+        let mainHtml = this.routes[page]();
+        let navHtml = this.navTemplate;
+
+        const navContainer = document.getElementsByTagName('nav')[0];
+        const mainContainer = document.getElementsByClassName('main-content')[0];
+
+        render(navHtml, navContainer);
+        render(mainHtml, mainContainer);
+    }
 }
 
-export const addToRoute = (uri, template) => {
-    routes[uri] = template;
-}
+// export const addNavHtml = (template) => {
+//     navTemplate = template;
+// }
 
-window.onhashchange = (e) => {
-    const locationHash = e.path[0].location.hash;
-    hashChangeHandler(locationHash)
-};
+// export const addToRoute = (url, template) => {
+//     routes[url] = template;
+// }
 
-export const hashChangeHandler = (locationHash) => {
-    const page = `/${locationHash}`;
-    let mainHtml = routes[page]();
-    let navHtml = navTemplate();
+// window.addEventListener('hashchange', (e) => {
+//     //const locationHash = e.path[0].location.hash;
+//     hashChangeHandler(e)
+// });
 
-    const navContainer = document.getElementsByTagName('nav')[0];
-    const mainContainer = document.getElementsByClassName('main-content')[0];
+// export const hashChangeHandler = (e) => {
+//     const locationHash = e.path[0].location.hash;
+//     const page = `/${locationHash}`;
+//     let mainHtml = routes[page]();
+//     let navHtml = navTemplate();
 
-    render(navHtml, navContainer);
-    render(mainHtml, mainContainer);
-}
+//     const navContainer = document.getElementsByTagName('nav')[0];
+//     const mainContainer = document.getElementsByClassName('main-content')[0];
+
+//     render(navHtml, navContainer);
+//     render(mainHtml, mainContainer);
+// }
 
 export const changePage = (page) => {
-    if (window.location.hash == page){
+    if (window.location.hash == page) {
         hashChangeHandler(window.location.hash);
     } else {
         window.location.href = `/#${page}`;;
-    } 
+    }
 }
 
 
