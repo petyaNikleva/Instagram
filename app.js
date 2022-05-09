dd814d93f5992b17df7a1e5ac1e3d2e5e33fe667
P N <petyanikleva@yahoo.com>
2022-05-09 18:05:10 +0300

    delete some additional code

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-import { navTemplate } from './pages/nav/navTemplate.js';
 import { getHeaderView } from './pages/header/header.js';
 import { getAdvertismentView } from './pages/advertisment/advertisment.js';
 import { getFooterView } from './pages/footer/footer.js';
 
-
 import { Router } from './router/router.js';
-import { newsFeedTemplate } from './pages/newsFeed/newsFeedTemplate.js';
-import { registerTemplate } from './pages/register/registerTemplate.js';
-import { loginTemplate } from './pages/login/loginTemplate.js';
-import { userListTemplate } from './pages/user-list/userListTemplate.js';
-
 
 const router = new Router();
 router.routes['/'];
