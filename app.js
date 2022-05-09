1c36bc18efb3bc1c3c18954d822e28cfa103bf51
P N <petyanikleva@yahoo.com>
2022-05-09 16:49:41 +0300

    Add nav view using class for routing

--- app.js
+++ app.js
@@ -10,14 +10,39 @@ import { registerTemplate } from './pages/register/registerTemplate.js';
 import { loginTemplate } from './pages/login/loginTemplate.js';
 import { userListTemplate } from './pages/user-list/userListTemplate.js';
 
-const router = new Router();
-router.addToRoute('/', newsFeedTemplate);
-router.addToRoute('/#register', registerTemplate);
-router.addToRoute('/#login', loginTemplate);
-router.addToRoute('/#user-list', userListTemplate);
 
-router.addNavHtml(navTemplate);
+const routes = {
+    '/': newsFeedTemplate,
+    '/#login': loginTemplate,
+    '/#register': registerTemplate,
+    '/#user-list': userListTemplate
+};
+const nav = {
+    '/': navTemplate,
+    '/#login': navTemplate,
+    '/#register': navTemplate,
+    '/#user-list': navTemplate
+};
+const router = new Router(routes, nav);
+router.routes['/'];
+router.routes['/#register'];
+router.routes['/#login'];
+router.routes['/#user-list'];
 
+router.nav['/'];
+router.nav['/#register'];
+router.nav['/#login'];
+router.nav['/#user-list'];
+
+// router.addToRoute('/', newsFeedTemplate);
+// router.addToRoute('/#register', registerTemplate);
+// router.addToRoute('/#login', loginTemplate);
+// router.addToRoute('/#user-list', userListTemplate);
+
+// router.addNavHtml('/', navTemplate);
+// router.addNavHtml('/#login', navTemplate);
+// router.addNavHtml('/#login', navTemplate);
+// router.addNavHtml('/#user-list', navTemplate);
 
 getHeaderView();
 getAdvertismentView();
--- router/router.js
+++ router/router.js
@@ -1,38 +1,69 @@
 import { render } from "../../node_modules/lit-html/lit-html.js";
 import { newsFeedTemplate} from "../pages/newsFeed/newsFeedTemplate.js"
 import { loginTemplate } from "../pages/login/loginTemplate.js";
-import {registerTemplate} from "../pages/register/registerTemplate.js";
+import { registerTemplate } from "../pages/register/registerTemplate.js";
+import { userListTemplate } from "../pages/user-list/userListTemplate.js";
+import { navTemplate } from "../pages/nav/navTemplate.js";
 
 //let navTemplate = {};
 
-export class Router {
-    constructor() {
-        this.routes = {};
-        this.navTemplate = {};
-        this.addToRoute('/', newsFeedTemplate);
-        this.addToRoute('/#login', loginTemplate);
-        this.addToRoute('/#register', registerTemplate);
 
-        this.addNavHtml(this.navTemplate);
+
+export class Router {
+    constructor(routes, nav) {
+        this.routes = {
+            '/': newsFeedTemplate,
+            '/#login': loginTemplate,
+            '/#register': registerTemplate,
+            '/#user-list': userListTemplate
+        };
+        this.nav = {
+            '/': navTemplate,
+            '/#login': navTemplate,
+            '/#register': navTemplate,
+            '/#user-list': navTemplate
+        };
+        // this.addToRoute('/', newsFeedTemplate);
+        // this.addToRoute('/#login', loginTemplate);
+        // this.addToRoute('/#register', registerTemplate);
+        // this.addToRoute('/#user-list', userListTemplate);
+
+        //this.addNavHtml(this.navTemplate);
+        // this.addNavHtml('/',navTemplate);
+        // this.addNavHtml('/#login', navTemplate);
+        // this.addNavHtml('/#register', navTemplate);
+        // this.addNavHtml('/#user-list', navTemplate);
 
         this.hashChangeHandler();
         window.addEventListener('hashchange', this.hashChangeHandler)
     }
 
-    addToRoute(url, template) {
-        this.routes[url] = template;
-    }
+    // addToRoute(url, template) {
+    //     this.routes[url] = template;
+    // }
 
-    addNavHtml(template) {
-        this.navTemplate = template;
-    }
+    // addNavHtml(uri, template) {
+    //     this.nav[uri] = template;
+    // }
 
     hashChangeHandler() {
         const locationHash = window.location.hash;
         //const locationHash = e.path[0].location.hash;
+        const routes = {
+            '/': newsFeedTemplate,
+            '/#login': loginTemplate,
+            '/#register': registerTemplate,
+            '/#user-list': userListTemplate
+        };
+        const nav = {
+            '/': navTemplate,
+            '/#login': navTemplate,
+            '/#register': navTemplate,
+            '/#user-list': navTemplate
+        };
         const page = `/${locationHash}`;
-        let mainHtml = this.routes[page]();
-        let navHtml = this.navTemplate;
+        let mainHtml = routes[page]();
+        let navHtml = nav[page]();
 
         const navContainer = document.getElementsByTagName('nav')[0];
         const mainContainer = document.getElementsByClassName('main-content')[0];
@@ -42,31 +73,6 @@ export class Router {
     }
 }
 
-// export const addNavHtml = (template) => {
-//     navTemplate = template;
-// }
-
-// export const addToRoute = (url, template) => {
-//     routes[url] = template;
-// }
-
-// window.addEventListener('hashchange', (e) => {
-//     //const locationHash = e.path[0].location.hash;
-//     hashChangeHandler(e)
-// });
-
-// export const hashChangeHandler = (e) => {
-//     const locationHash = e.path[0].location.hash;
-//     const page = `/${locationHash}`;
-//     let mainHtml = routes[page]();
-//     let navHtml = navTemplate();
-
-//     const navContainer = document.getElementsByTagName('nav')[0];
-//     const mainContainer = document.getElementsByClassName('main-content')[0];
-
-//     render(navHtml, navContainer);
-//     render(mainHtml, mainContainer);
-// }
 
 export const changePage = (page) => {
     if (window.location.hash == page) {
