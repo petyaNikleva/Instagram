571183e336df7746c31cad401befca67a58c94f7
P N <petyanikleva@yahoo.com>
2022-05-20 12:16:15 +0300

    Upload on profile page photo without redirect to local host, but still reload the page

--- server/server.js
+++ server/server.js
@@ -1,28 +1,15 @@
-
-    
-
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
 
-//import authService from "../services/authenticationService.js"
-
-//import { storage } from '../middleware/configMulter.js';
-
 const app = express();
 
 const storage = multer.diskStorage({
-    
     destination: (req, file, cb) => {
         cb(null, 'uploads');
     },
-
     filename: (req, file, cb) => {
-        //console.log(file);
-        //console.log(req.body);
         cb(null, Date.now() + path.extname(file.originalname))
-        //cb(null, '123' + path.extname(file.originalname))
-
     }
 })
 
@@ -31,13 +18,12 @@ const upload = multer({
 })
 
 
-app.get('/upload', (req, res) => {
-    res.render('upload');
-});
+// app.get('/upload', (req, res) => {
+//     res.render('upload');
+// });
 
 app.post('/upload', upload.single('image'), (req, res) => {
-    console.log(res)
-    //console.log(res.file.filename); - it is undefined
+    console.log(req.file)
     res.send('Image uploaded');
 });
 
