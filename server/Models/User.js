ce2179d8cc288f57c1cfda4f180b9ab839229eab
P N <petyanikleva@yahoo.com>
2022-06-06 11:37:45 +0300

    Add property 'unique: true' to the userSchema

--- server/Models/User.js
+++ server/Models/User.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    email: {type:String,
-    required:true},
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
     firstName: String,
     lastName: String,
     dateOfBirth: String,
     password: String,
-    image: String,
-    isLogged: {
-        type: "String",
-        default: "No"
-    }
+    image: String
 })
 
 module.exports = mongoose.model("User", userSchema)
