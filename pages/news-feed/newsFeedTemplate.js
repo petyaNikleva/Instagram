78c6e1033a2c983eff2899e4987d621d8d6467df
P N <petyanikleva@yahoo.com>
2022-06-27 12:25:51 +0300

    Еxtracted news-feed logic in а separate file

--- pages/news-feed/newsFeedTemplate.js
+++ pages/news-feed/newsFeedTemplate.js
@@ -1,8 +1,6 @@
 import { html, render } from "./../../node_modules/lit-html/lit-html.js";
-import { getAll, update } from "../../services/postService.js"
-import authService from "../../services/authenticationService.js";
-
-const baseUrl = 'http://localhost:3000'
+import { getAll } from "../../services/postService.js"
+import { postViewHandler, likersCountHandler, likeClickHandler } from "./news-feed.js";
 
 
 function allposts () {
@@ -29,64 +27,7 @@ export let newsFeedTemplate = () => html`
         }
 </section>
 `
-
-function postViewHandler(post) {
-    const imageId = post.image;
-    let data;
-    fetch(`${baseUrl}/upload/${imageId}`)
-        .then(response => {
-            response.blob()
-                .then(blobResponse => {
-                    data = blobResponse;
-                    const urlCreator = window.URL || window.webkitURL;
-                    const imgBlob = urlCreator.createObjectURL(data);
-                    const imgElement = document.getElementsByClassName(post._id)[0];
-                    imgElement.setAttribute('src', imgBlob);
-                    authorHandler(post._authorId);
-                })
-         })
-        .catch((err) => {
-            console.log(err)
-        });
-    
-}
-
-
-function authorHandler(_authorId) {
-     fetch(`${baseUrl}/${_authorId}`)
-     .then( res => res.json())
-     .then(user => {
-        const authorHTMLCollection = document.getElementsByClassName(_authorId);
-        let authorElements = [...authorHTMLCollection];
-        authorElements.forEach(element => {
-            element.textContent = `Author: ${user.firstName} ${user.lastName}`
-        } )
-    })
-    .catch((err) => {
-        console.log(err)
-    }) 
-}   
-
-function likersCountHandler (postId) {
-    fetch(`${baseUrl}/postId/${postId}`)
-    .then( res => res.json())
-    .then(post => {
-       const likers = post.likes;
-       const currentUser = authService.getLoggedUser();
-       const likeDiv = document.getElementsByClassName(`likes-${post._id}`)[0];
-       if (likers.includes(currentUser._id) && (post.likes.length) > 0) {
-            likeDiv.textContent = `Liked by You and ${(post.likes.length)-1} people.`     
-       }  else if (likers.includes(currentUser._id)) {
-        likeDiv.textContent = `Liked by You.`
-        } else {
-            likeDiv.textContent = `Liked by ${(post.likes.length)} people.`
-        } 
-    })
-    .catch(err => {
-        console.log(err);
-    }) 
-}
-
+ 
 export let postTemplate = (post) => html`
     <article class="post-card posts">
         <div class="card-img">
@@ -103,38 +44,6 @@ export let postTemplate = (post) => html`
         </div>
     </article>
 `
-
-function likeClickHandler(e, postId) {
-    e.preventDefault();
-    fetch(`${baseUrl}/postId/${postId}`)
-     .then( res => res.json())
-     .then(post => {
-        const likers = post.likes;
-        const currentUser = authService.getLoggedUser();
-        if (currentUser.user === 'noUser') {
-            alert('Only logged users can like posts.');
-            return;
-        } else if (likers.includes(currentUser._id)) {
-            alert('You have already liked this post.')
-            return;
-        } else {
-            likers.push(currentUser._id);
-            post.likes = likers;
-            console.log(post);
-            update(postId, post)
-            .then((updatedPost) => {
-                const likeDiv = document.getElementsByClassName(`likes-${post._id}`)[0];
-                (post.likes.length) - 1 == 0
-                ?  likeDiv.textContent = `Liked by You`   
-                :  likeDiv.textContent = `Liked by You and ${(post.likes.length)-1} people.`
-              })
-        }  
-    })
-    .catch((err) => {
-        console.log(err)
-    }) 
-   
-}
 
 
 
