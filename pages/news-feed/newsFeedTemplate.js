c6f438c09a17d7993ee87d99ee56e2c74fd1a5c7
P N <petyanikleva@yahoo.com>
2022-06-29 17:12:40 +0300

    Extract comment logic in a separate file comments.js

--- pages/news-feed/newsFeedTemplate.js
+++ pages/news-feed/newsFeedTemplate.js
@@ -1,9 +1,8 @@
 import { html, render } from "./../../node_modules/lit-html/lit-html.js";
 import { getAll } from "../../services/postService.js"
 import { postViewHandler, likersCountHandler, likeClickHandler } from "./news-feed.js";
-import authService from "../../services/authenticationService.js";
-import { create } from "../../services/commentService.js";
-import { update } from "../../services/postService.js"
+import { commentClickToggle, addCommentHandler } from "./comments.js"
+
 
 function allposts () {
     getAll()
@@ -62,46 +61,6 @@ export let postTemplate = (post) => html`
     </article>
 `
 
-function commentClickToggle (e, postId) {
-    e.preventDefault();
-    const commentElement = document.getElementsByClassName(`comments-container-${postId}`)[0];
-    const showCommentTextEleement = document.getElementsByClassName(`show-${postId}`)[0];
-    
-    if (showCommentTextEleement.textContent === 'Show comments') {
-        commentElement.style.display = "block"
-        showCommentTextEleement.textContent = 'Hide comments'
-    } else if (showCommentTextEleement.textContent === 'Hide comments') {
-        commentElement.style.display = "none";
-        showCommentTextEleement.textContent = 'Show comments'
-    }    
-}
-
-function addCommentHandler(e, post) {
-    e.preventDefault();
-    const text = document.getElementById(`addComment-${post._id}`).value;
-    const currentUser = authService.getLoggedUser();
-    const userId = currentUser._id;
-    if (currentUser.user === 'noUser') {
-        alert ('Only logged users can add comments.');
-        return;
-    }
-    create(text, userId)
-        .then((comment) => {
-            const commentId = comment._id;
-            post.comments.push(commentId);
-            update(post._id, post)
-                .then(data => {
-                    console.log(data);
-            })
-            setTimeout(() => {
-                alert('Your comment has been added.')
-                window.location.href = "/#";
-            }, 500);
-        })
-        .catch((err) => {
-        console.log(err);
-    })
-}
 
 
           
